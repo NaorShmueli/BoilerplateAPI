3a0572f41afe8ea481e8333d2ad32497abfaf7d7
naor2 <naor2@DESKTOP-2DV3RHL>
2021-07-15 18:55:16 +0300

    init

---
+++ BoilerplateAPI/Tests/TestExample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class TestExample : UnitTestBase
+    {
+        public TestExample() : base()
+        {
+
+        }
+
+        [Fact]
+        public void Example()
+        {
+            ////Arrange
+            //_someMock.Setup(x => x.Method1(It.IsAny<YourParamType>())).Returns(YourResultCase);
+            //var msg = SomeData();
+
+            ////Act
+            //var result = _actualTestService.SomeTestMethod(msg);
+
+            ////Assert
+            //Assert.True(result, "Info");
+        }
+    }
+}
---
+++ BoilerplateAPI/Tests/UnitTestBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class UnitTestBase
+    {
+        private readonly MockRepository _mockRepository;
+        private readonly IConfiguration _configuration;
+        public UnitTestBase()
+        {
+            _mockRepository = new MockRepository(MockBehavior.Default);
+            _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.test.json").Build();
+            //Create your interfaces
+        }
+
+    }
+}
